683787b7fe08c36dfa18a6054f1e840fc031f32c
Adrian Ferenc <68762863+adrianferenc@users.noreply.github.com>
2021-07-22 12:33:28 -0700

    fixed bug

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -3,6 +3,7 @@ import * as usersAPI from './users-api';
 export async function signUp(userData) {
     try {
       const token = await usersAPI.signUp(userData);
+      localStorage.setItem('token', token);
       return token;
     } catch {
       throw new Error('Invalid Sign Up');

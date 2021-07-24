cecc88170884781ba554fccaca7f9079896e66a4
adrianferenc <68762863+adrianferenc@users.noreply.github.com>
2021-07-23 17:11:35 -0700

    Update users-api.js
    Simplified code

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,39 +1,14 @@
 import { getToken } from "./users-service";
 const BASE_URL = "/api/users";
 
-export async function signUp(userData) {
-  const res = await fetch(BASE_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userData),
-  });
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Invalid Sign Up");
-  }
+
+export function signUp(userData) {
+  return sendRequest(BASE_URL, 'POST', userData);
 }
 
-export async function login(credentials) {
-  const user = await fetch(`${BASE_URL}/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(credentials),
-  });
-  if (user.ok) {
-    return user.json();
-  } else {
-    throw new Error("Invalid Login");
-  }
+export function login(credentials) {
+  return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
 }
-
-// export function signUp(userData) {
-//   return sendRequest(BASE_URL, 'POST', userData);
-// }
-
-// export function login(credentials) {
-//   return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
-// }
 
 export function checkToken() {
   return sendRequest(`${BASE_URL}/check-token`);
